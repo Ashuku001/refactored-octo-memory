f15c202162392f368dcbde60125f4ba6b2d0aa53
ashuku001 <ashukuezra@gmai.com>
2024-08-13 23:26:00 +0300

    added seed

---
+++ server/src/seeders/20240603061618-fake-store.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Stores",
+      [
+        {
+          id: 1,
+          name: "My Fake Store",
+          merchantId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkDelete("Stores", null, {});
+  },
+};
